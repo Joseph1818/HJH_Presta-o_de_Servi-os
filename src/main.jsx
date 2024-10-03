974a569f194815744560e6d87337934f35dd2e66
MukunaChristian <christianmbuyimukuna@gmail.com>
2024-10-03 18:12:15 +0200

    add the scss and nav bar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import WebLayout from "./layouts/WebLayout";
+import Home from "./pages/Home";
+import "./scss/pages/home.scss";
+import "./styles/main.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <Router>
+    <Routes>
+      <Route element={<WebLayout />}>
+        <Route path="" element={<Home/>} />
+      </Route>
+    </Routes>
+  </Router>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+function Home() {
+
+
+    return (
+      <>
+ <header className="top-bar-main" >
+  <div className="top-bar">
+      <div className="logo">
+        <img src="/logo.png" alt="Logo" />
+      </div>
+      <nav className="navigation">
+        <a href="#">Home</a>
+        <a href="#">Service</a>
+        <a href="#">Production</a>
+        <a href="#">Contact Us</a>
+      </nav>
+      </div>
+    </header>
+      </>
+    )
+  }
+  
+  export default Home
