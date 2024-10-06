b39a6c7351c9aa0b895868c1a7560d0a0c6e6f48
MukunaChristian <christianmbuyimukuna@gmail.com>
2024-10-06 12:19:58 +0200

    added animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import whatsapp from "../assets/whatsapp.png"
+import { useInView } from 'react-intersection-observer';
+
+
 import logo from '../assets/logo.jpg'; // Ensure this path is correct
 
 function Home() {
@@ -10,6 +13,13 @@ function Home() {
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+
+
+
+  const { ref, inView } = useInView({
+    threshold: 0.1, // Trigger when 10% of the element is in view
+  });
 
   return (
     <>
@@ -41,7 +51,7 @@ function Home() {
           <h1>Deixe seu ambiente sempre fresco e saudável!</h1>
           <p>Venda, instalação e manutenção de ar-condicionado com quem entende do assunto.</p>
 
-          <button className="contact-button-main">
+          <button className="contact-button">
             <img src={whatsapp} className="icon-whastapp" alt="WhatsApp" />
             ENTRE EM CONTATO
           </button>
@@ -49,60 +59,66 @@ function Home() {
 
         
       </div>
-     
+      <section ref={ref} className={` service box ${inView ? 'visible' : ''}`} >
+    <div className='child'>
+        <div className="image-container">
+            <img src={logo} className="logo2" alt="Service 1" />
+        </div>
+        <p>A equipe qualificada da Só Frio realiza a instalação de
+          todos os tipos de equipamentos condicionadores de ar.
+          Realizamos vistoria no local de instalação para o correto
+          dimensionamento do tipo de equipamento.
+        </p>
+    </div>
+    <div className='child'>
+        <div className="image-container">
+            <img src={logo} className="logo2" alt="Service 2" />
+        </div>
+        <p>
+          Não deixe para fazer a manutenção de seu equipamento somente depois que ele quebrou.
+          Com a manutenção preventiva, você aumenta a vida útil de seu equipamento mantendo seus
+          componentes em perfeito estado.
+        </p>
+    </div>
+    <div className='child'>
+        <div className="image-container">
+            <img src={logo} className="logo2" alt="Service 3" />
+        </div>
+        <p>
+          A Só Frio possui a mais completa equipe de profissionais, com comprovada
+          experiência, para fornecer e instalar sistemas de HVAC centrais desde
+          pequenos sistemas até obras comerciais/indústrias complexas.
+        </p>
+    </div>
+    <div className='child'>
+        <div className="image-container">
+            <img src={logo} className="logo2" alt="Service 4" />
+        </div>
+        <p>Na Só Frio você encontra uma grande variedade de equipamentos de ar
+          condicionados, além do melhor atendimento e condições exclusivas. Entre
+          em contato com nossa equipe e confira nossos modelos.
+        </p>
+    </div>
+    <div className='child'>
+        <div className="image-container">
+            <img src={logo} className="logo2" alt="Service 5" />
+        </div>
+        <p>
+          Para saber mais sobre o sistema VRF/VRV, entre em contato
+          e solicite uma visita Técnica com nossos Engenheiros. Para
+          maiores informações, entre em contato com nossa equipe de atendimento.
+        </p>
+    </div>
+</section>
+
+
+
+
+
+
       </main>
 
-      <section className='main-service'>
-          <div className='child1'>
-            <img src={logo} className="logo2" alt="" />
-            <p>A equipe qualificada da Só Frio realiza a instalação de
-              todos os tipos de equipamentos condicionadores de ar.
-              Realizamos vistoria no local de instalação para o correto
-              dimensionamento do tipo de equipamento.
-            </p>
-
-          </div>
-          <div className='child2'>
-          <img src={logo} className="logo2" alt="" />
-
-            <p>
-              Não deixe para fazer a manutenção de seu equipamento somente depois que ele quebrou.
-              Com a manutenção preventiva, você aumenta a vida útil de seu equipamento mantendo seus
-              componentes em perfeito estado.
-            </p>
-
-          </div>
-          <div className='child3'>
-          <img src={logo} className="logo2" alt="" />
-
-            <p>
-              A Só Frio possui a mais completa equipe de profissionais, com comprovada
-              experiência, para fornecer e instalar sistemas de HVAC centrais desde
-              pequenos sistemas
-              até obras comerciais/industrias complexas.
-            </p>
-          </div>
-          <div className='child4'>
-          <img src={logo} className="logo2" alt="" />
-
-            <p>Na Só Frio você encontra uma grande variedade de equipamentos de ar
-              condicionados, além do melhor atendimento e condições exclusiva. Entre
-              em contato com
-              nossa equipe e confira nossos modelos.</p>
-          </div>
-          <div className='child5'>
-          <img src={logo} className="logo2" alt="" />
-
-            <p>
-            Para saber mais sobre o sistema VRF/VRV, entre em contato 
-            e solicite uma visita Técnica com nossos Engenheiros. Para
-            maiores informações, entre em 
-            contato com nossa equipe de. atendimento
-            </p>
-
-          </div>
-
-        </section>
+      
     </>
   );
 }
